81751d44adf1ecbca0879ef39f2f7e87d230f815
Subhodip Deti <subhodipdeti1414@gmail.com>
2021-05-19 13:27:45 +0530

    desc fixed

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,31 +1,26 @@
 const title = 'A.S. Fabrication and Engineering Works';
 const description =
-  'Learn how to build static and server-rendered applications with Next.js and React.';
+  'At ‘A.S. Fabrication and Engineering Works’ we will likely give you the best fix encounter conceivable and give you an affable and practical option in contrast to your new vehicle merchant. You can think of us as a top of the line office that is agreeable, cordial, and moderate. We treat our clients like family since we comprehend the significance of genuineness and trustworthiness in the automobile industry. Your trust isn’t something we mess with, and each time you approach us we guarantee to surpass desires.';
 
 const SEO = {
   title,
   description,
-  canonical: 'https://masteringnextjs.com',
+  canonical: 'https://firebasestorage.googleapis.com/v0/b/neoium-5477b.appspot.com/o/posts%2Ft3.svg?alt=media&token=19924c73-c2ce-42a0-b8e4-a35b3d84119b',
   openGraph: {
     type: 'website',
     locale: 'en_IE',
-    url: 'https://masteringnextjs.com',
+    url: 'https://firebasestorage.googleapis.com/v0/b/neoium-5477b.appspot.com/o/posts%2Ft3.svg?alt=media&token=19924c73-c2ce-42a0-b8e4-a35b3d84119b',
     title,
     description,
     images: [
       {
-        url: 'https://masteringnextjs.com/og.png',
+        url: 'https://firebasestorage.googleapis.com/v0/b/neoium-5477b.appspot.com/o/posts%2Ft3.svg?alt=media&token=19924c73-c2ce-42a0-b8e4-a35b3d84119b',
         alt: title,
         width: 1200,
         height: 700
       }
     ]
   },
-  twitter: {
-    handle: '@leeerob',
-    site: '@leeerob',
-    cardType: 'summary_large_image'
-  }
 };
 
 export default SEO;
